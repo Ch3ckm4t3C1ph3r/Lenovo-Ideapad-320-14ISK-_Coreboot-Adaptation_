1dcd844b99465221cf93d145ef648fb9178b2df7
Ch3ckm4t3C1ph3r <4rtur02ee@protonmail.com>
2026-05-14 20:48:15 +0000

    Update Makefile to simplify ramstage inclusion
    Removed conditional inclusion of cfr.c and gma-mainboard.ads from ramstage.

--- Coreboot Adaptation Code/Makefile.mk
+++ Coreboot Adaptation Code/Makefile.mk
@@ -4,7 +4,5 @@ bootblock-y += bootblock.c
 
 romstage-y += memory_init_params.c
 
-ramstage-$(CONFIG_DRIVERS_OPTION_CFR) += cfr.c
 ramstage-y += gpio.c
 ramstage-y += ramstage.c
-ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads
