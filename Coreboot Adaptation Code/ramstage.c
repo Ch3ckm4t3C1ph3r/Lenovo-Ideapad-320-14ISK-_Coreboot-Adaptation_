90762ed82c37cd50be50e950a1161a664dc95adf
Ch3ckm4t3C1ph3r <4rtur02ee@protonmail.com>
2026-05-27 04:01:36 +0000

    Refactor GPIO setup and mainboard operations
    Updated comments and removed unused function for mainboard operations.

--- Coreboot Adaptation Code/ramstage.c
+++ Coreboot Adaptation Code/ramstage.c
@@ -5,18 +5,8 @@
 
 void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 {
-	// Setup GPIOs
-}
-
-
-static void mainboard_enable(struct device *dev)
-{
-	if (CONFIG(VARIANT_HAS_DGPU)) {
-		dgpu_detect();
-		dev->ops->acpi_fill_ssdt = ssdt_add_dgpu;
-	}
+	/* Setup GPIOs */
 }
 
 struct chip_operations mainboard_ops = {
-	.enable_dev = mainboard_enable,
 };
